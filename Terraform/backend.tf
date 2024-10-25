799e2e91bdac55ef8059302c4480c9b505417a0a
SaurabhRNalawade <saurabhnalawade2112@gmail.com>
2024-10-25 17:58:03 +0530

    Create backend.tf

---
+++ Terraform/backend.tf
@@ -0,0 +1,13 @@
+#Terraform Remote backend
+terraform{
+  backend "s3"{
+  bucket="s3-remote-backend"
+#filename--> key="terraform.tfstate"
+  region="ap-south-1"
+  profile="saurabh"
+
+
+#Return to local --> terraform init -migrate-state
+terraform {
+  backend "local"{
+  path="terraform.tfstate"
